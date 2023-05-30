10bd613078ba12dcf135e96283b49fc5dcedcdc2
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2023-05-30 16:15:20 -0600

    first commit

---
+++ src/main/java/mx/egd/sat/descargopagoanalizer/DescargopagoanalizerApplication.java
@@ -0,0 +1,13 @@
+package mx.egd.sat.descargopagoanalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescargopagoanalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescargopagoanalizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/egd/sat/descargopagoanalizer/DescargopagoanalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.egd.sat.descargopagoanalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescargopagoanalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
